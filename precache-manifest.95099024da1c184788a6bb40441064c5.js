4ca6043a46ea88402cade2805eb5f18bea44e865
katrinapo <emailkatrinapo@gmail.com>
2020-09-15 16:26:24 -0400

    Updates

---
+++ precache-manifest.95099024da1c184788a6bb40441064c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee2f90ef8866a7e13fafb1f25022d6c",
+    "url": "/React-Fetch-Videos-From-YoutubeAPI/index.html"
+  },
+  {
+    "revision": "b6462365424ae9f5629f",
+    "url": "/React-Fetch-Videos-From-YoutubeAPI/static/css/main.96c11df6.chunk.css"
+  },
+  {
+    "revision": "3763f2051ea5c562848a",
+    "url": "/React-Fetch-Videos-From-YoutubeAPI/static/js/2.54894ef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Fetch-Videos-From-YoutubeAPI/static/js/2.54894ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6462365424ae9f5629f",
+    "url": "/React-Fetch-Videos-From-YoutubeAPI/static/js/main.df090a49.chunk.js"
+  },
+  {
+    "revision": "360c93fe6ba00f31f5ea",
+    "url": "/React-Fetch-Videos-From-YoutubeAPI/static/js/runtime-main.544a2ff8.js"
+  }
+]);
